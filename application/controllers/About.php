000a32f4f2bbe559f619b146a26ca0adb125deed
Denis R <denis.ristic3@gmail.com>
2019-05-27 21:09:33 +0200

    Added new page. << about me >>

---
+++ application/controllers/About.php
@@ -0,0 +1,57 @@
+<?php 
+
+defined("BASEPATH") or exit("No direct access to script!");
+
+class About extends CI_Controller{
+
+    public function index(){
+
+        /**
+        *
+        *	Load all settings for website
+        *	This is place where we can include
+        *	all data we want to show to user before
+        *	we can go to create a view!!!!
+        *
+        */
+        $this->load->model("Cat_model");
+        $this->load->model("Post_model");
+    
+        $cats = $this->Cat_model->listCats();
+        $posts= $this->Post_model->listPublicPosts();
+    
+        if( $this->session->userdata(md5("user_rblog")) ){
+            $posts = $this->Post_model->listAllPosts();
+        } else{
+            $posts = $this->Post_model->listPublicPosts();
+        }
+    
+        /**
+        *
+        *	Load view
+        *	After we preapre all data for
+        *	website display it here...
+        *
+        */
+        $this->load->view( "about_me", [
+    
+            // Load file header
+            "header" => $this->load->view("header", false, true),
+    
+            // Load footer
+            "footer" => $this->load->view("footer", false, true),
+    
+            // Load login Bar
+    
+            "login_bar" => $this->load->view("login-bar", false, true),
+    
+            // Load main menu
+            "main_menu" => $this->load->view("main_menu", array( "cats" => $cats ), true),
+    
+            "posts" => $posts
+    
+        ] );
+        
+    }
+
+}
---
+++ application/views/about_me.php
@@ -0,0 +1,66 @@
+<?php 
+
+/**
+*
+*	Load header file
+*	This file contain all style or js
+*	scripts we have..
+*
+*/
+
+echo $header;
+
+?>
+
+<div class="container-fluid h-100">
+	<div class="col-sm-3 left-side">
+		<div class="blog-menu">
+			<div class="button-menu">
+				<div class="search-box">
+					<form method="GET" action="<?php echo site_url('search'); ?>">
+						<input type="search" name="q" placeholder="Pretraga" />
+						<button><span class="glyphicon glyphicon-search"></span></button>
+					</form>
+				</div>
+			</div>
+			<div class="main-menu mobile-select">
+				<?php echo $main_menu; ?>
+			</div>
+			<div class="bottom-menu m-font">
+				<h1>rblog.com</h1>
+				<p>A journey of a thousand miles begins with a single step</p>
+				<h6><small>&copy; All copyrights reserved!</small></h6>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-9 right-side">
+		<div class="row">
+			<?php echo $login_bar; ?>
+		</div>
+
+		<!-- Page Content -->
+		<div class="row main-profile">
+			<div class="col-sm-12">
+                U pripremi
+			</div>
+		</div>
+		<!-- Page Content EOF -->
+
+	</div>
+</div>
+
+<?php 
+
+/**
+*
+*	End page with footer script
+*	this will execute on every end
+*	We loading file
+*
+*	@ /views/footer.php
+*
+*/
+
+echo $footer;
+
+?>
